2073b704211ac06ee35d21ee74bf2367ab8ed24d
start.vaadin.com <no-reply@vaadin.com>
2024-09-18 07:46:46 +0000

    Generated project

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,26 @@
+package com.example.application;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@NpmPackage(value = "@fontsource/nunito-sans", version = "4.5.0")
+@Theme(value = "my-app", variant = Lumo.DARK)
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
